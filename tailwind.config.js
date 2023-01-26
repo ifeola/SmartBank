0d60bfb9f021ef22dab00c55ef9620f186ac54c6
DS Ifeola <nomorelimit11@gmail.com>
2023-01-26 03:51:58 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px",
+      xxl: "2560px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["General sans", "Montserrat"],
+        serif: ["ui-serif", "Georgia"],
+      },
+      colors: {
+        primaryColor: "#5A45FE",
+        secondaryColor: "#0F0B2E",
+        bodyFont: "#0F0B2E",
+        darkBlue: "rgb(15,23,42)",
+      },
+    },
+  },
+  plugins: [],
+};
